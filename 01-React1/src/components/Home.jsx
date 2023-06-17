6560fabda9e222dc8d755a9248592c8f2c447bf5
Nishant Giri <nishantgiri19722000@gmail.com>
2023-06-17 21:33:21 +0530

    components/Home.jsx : home1-coded

---
+++ 01-React1/src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import vg from "../assets/2.png";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home" id="Home">
+        <main>
+          <h1>Hello React</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Home;
