7fb8f42c244a65f3ed6337c148f60aa0051be67f
Nishant Giri <nishantgiri19722000@gmail.com>
2023-06-20 09:41:54 +0530

    components/Home.jsx : home4-added

--- 01-React1/src/components/Home.jsx
+++ 01-React1/src/components/Home.jsx
@@ -44,6 +44,49 @@ const Home = () => {
           </p>
         </div>
       </div>
+
+      <div className="home4" id="Brands">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+
+            <div
+              style={{
+                animationDelay: "1s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
     </>
   );
 };
