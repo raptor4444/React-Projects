3a6c610803b127cc79eab76d20b14ff3dc0db1d6
Nishant Giri <nishantgiri19722000@gmail.com>
2023-06-24 22:30:24 +0530

    components/Footer.jsx : footer-coded

---
+++ 01-React1/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Techystar</h1>
+        <p>@all rights reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow me</h5>
+        <div>
+          <a href="https://github.com/raptor4444" target="_blank">
+            Github
+          </a>
+          <a href="https://www.linkedin.com/in/raptor4444/" target="_blank">
+            Linkedin
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
